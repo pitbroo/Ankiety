209f31a1107f1cba3f00ef3a74fa953861a50dfe
Rafał Balcerzak <rafalbalcerzak98@gmail.com>
2021-03-27 19:45:16 +0100

    Dodanie do github

---
+++ src/main/java/com/ankiety/ankiety/AnkietyApplication.java
@@ -0,0 +1,13 @@
+package com.ankiety.ankiety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnkietyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnkietyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ankiety/ankiety/AnkietyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankiety.ankiety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnkietyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
