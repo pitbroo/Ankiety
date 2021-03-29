4ff08bb7f2f374d30b01bf37b0d49b78cdfdb670
Piotr <pitbroo@gmail.com>
2021-03-29 21:44:19 +0200

    Dodano spacje

--- src/main/java/com/ankiety/ankiety/model/OdpowiedziOsob.java
+++ src/main/java/com/ankiety/ankiety/model/OdpowiedziOsob.java
@@ -14,6 +14,7 @@ public class OdpowiedziOsob {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idOdpowiedzi;
+
     @ManyToOne
     @JoinColumn(name = "id_osoby")
     private Osoby osoby;
